913cf469b5b00a031ef08939324707eba4a461cf
rittichai.hiranlikit <rittichai.hiranlikit@accenture.com>
2019-04-30 00:29:06 +0700

    add config log4j2

--- src/main/java/com/bezy/demo/Controller/GreetingController.java
+++ src/main/java/com/bezy/demo/Controller/GreetingController.java
@@ -1,6 +1,8 @@
 package com.bezy.demo.Controller;
 
 import com.bezy.demo.hello.Greeting;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,9 +14,14 @@ public class GreetingController {
 
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
+    private static final Logger logger = LoggerFactory.getLogger(GreetingController.class);
+
 
     @GetMapping("/greeting")
     public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        logger.info("info",name);
+        logger.debug("debug",name);
+        logger.error("error",name);
         return new Greeting(counter.incrementAndGet(),
                 String.format(template, name));
     }
